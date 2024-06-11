d79a4f0b59dea39c0fe5e77e3c6a3a6c71b67e10
SadraBardareh <Sadra.bardareh@gmail.com>
2024-06-11 17:38:38 +0330

    supervisor command changed

--- lapizua/tasks.py
+++ lapizua/tasks.py
@@ -6,6 +6,5 @@ import datetime
 @shared_task(name='delete_old_lapizua_pockets')
 def delete_old_lapizua_pockets():
     d = timezone.now() - datetime.timedelta(hours=24)
-    print(d)
     pockets = LapizuaPocket.objects.filter(created_at__lt=d)
     pockets.delete()
