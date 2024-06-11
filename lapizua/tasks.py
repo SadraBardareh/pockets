6f771eb76c72f961abc70d6def5f8af3d476879d
SadraBardareh <Sadra.bardareh@gmail.com>
2024-06-11 17:38:01 +0330

    supervisor command changed

--- lapizua/tasks.py
+++ lapizua/tasks.py
@@ -3,8 +3,9 @@ from celery import shared_task
 from django.utils import timezone
 import datetime
 
-@shared_task
+@shared_task(name='delete_old_lapizua_pockets')
 def delete_old_lapizua_pockets():
     d = timezone.now() - datetime.timedelta(hours=24)
+    print(d)
     pockets = LapizuaPocket.objects.filter(created_at__lt=d)
     pockets.delete()
